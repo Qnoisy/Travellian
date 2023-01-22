910d5d7f3a339c6740a1439843797814271d4327
Khmelyuk <skhmelyuk1985@gmail.com>
2023-01-22 18:05:06 +0200

    verstka bloka destination

--- js/script.js
+++ js/script.js
@@ -13,3 +13,22 @@ listMenu.forEach((item) => {
     e.target.parentElement.classList.toggle("active")
   })
 })
+
+const swiper = new Swiper(".destination-swiper", {
+  direction: "horizontal",
+  loop: true,
+  spaceBetween: 32,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".destination-swiper__button.swiper-button-next",
+    prevEl: ".destination-swiper__button.swiper-button-prev",
+  },
+
+  breakpoints: {
+    320: { slidesPerView: 1 },
+    768: { slidesPerView: 2.2 },
+    1152: { slidesPerView: 2.8 },
+    1420: { slidesPerView: 3.4 },
+  },
+})
